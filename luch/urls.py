c1c4a3e1b011cdd1112fd0d43f51e498cb2fadea
Samuel Useche <samueluseche87@gmail.com>
2023-07-13 20:11:44 -0500

    first commit

--- luch/urls.py
+++ luch/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gsadashboard import views
+
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('gsadashboard.urls')),
+    path('/', views.index, name='index'),
+    path('sync/', views.sync_rfq, name='sync'),
+    path('list_rfqs/', views.rfqList, name='listRfqs'),
+    path('rfq-view/<int:id>', views.rfqView,name="refqView"),
+    path('sigin/', views.signin,name="signin"),
+    path('signup/', views.signup,name="signup"),
+    path('logout/', views.signout, name='logout'),
 ]
+
