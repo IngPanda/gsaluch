ef78b1d39647417c08c62282bcaaae6cc43cb0fc
Samuel Useche <samueluseche87@gmail.com>
2023-07-13 20:19:32 -0500

    change url

--- gsadashboard/urls.py
+++ gsadashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('sync/', views.sync_rfq, name='sync'),
     path('list_rfqs/', views.rfqList, name='listRfqs'),
     path('rfq-view/<int:id>', views.rfqView,name="refqView"),
